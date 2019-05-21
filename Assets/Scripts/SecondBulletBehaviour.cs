13cfb43ce1774804e79763dc5c84cf3c9c8ed5c1
Jordon Cotton <s188044@students.aie.edu>
2019-05-21 14:20:04 -0500

    Second bullet behaviour

---
+++ Assets/Scripts/SecondBulletBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondBulletBehaviour : MonoBehaviour
+{
+    public GameObject bullet_emiiter;
+    public GameObject bullet;
+
+    public float bullet_Forward_Force;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("space")) ;
+
+        GameObject Temporary_Bullet_Handler;
+        Temporary_Bullet_Handler = Instantiate(bullet, bullet_emiiter.transform.position, bullet_emiiter.transform.rotation) as GameObject;
+
+        Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+        Rigidbody Temporary_rigidbody;
+        Temporary_rigidbody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+        Temporary_rigidbody.AddForce(transform.forward * bullet_Forward_Force);
+
+        Destroy(Temporary_Bullet_Handler, 10.0f);
+	}
+}
