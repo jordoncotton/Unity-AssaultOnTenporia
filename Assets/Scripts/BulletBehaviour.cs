0bd8d1cd626449126b612aac52d303e77abfc3f6
Jordon Cotton <s188044@students.aie.edu>
2019-05-21 11:23:43 -0500

    BulletBehaviour

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public float bulletSpeed = 10;
+
+	// Use this for initialization
+	void Fire()
+    {
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
