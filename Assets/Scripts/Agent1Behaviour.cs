95d4ef8674ae7d11fa87314050e94013bfef6716
Lodis Perkins <s188043@students.aie.edu>
2019-05-22 16:11:45 -0500

    Updated gitignore

--- Assets/Scripts/Agent1Behaviour.cs
+++ Assets/Scripts/Agent1Behaviour.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 public class Agent1Behaviour : MonoBehaviour {
-    private Vector3 playerinput; 
+    private Vector3 playerinput;
+    [SerializeField]
+    private GameObject ammo;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,6 +16,9 @@ public class Agent1Behaviour : MonoBehaviour {
         playerinput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = transform.position+=playerinput;
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate<GameObject>(ammo);
+        }
     }
 }
